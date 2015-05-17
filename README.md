9fb7230a7a62c1fafb2eeb8dde2ebe6b64e4e04f
Maksim Pecherskiy <max@maksimize.com>
2015-05-17 10:24:54 -0700

    Adding some process to README, initial data loading for commons.

---
+++ README.md
@@ -0,0 +1,36 @@
+# Getting Started
+## Environment
+OS Version
+R Version
+R Studio Version
+## Package Deps
+? Try to install them automatically?
+## Files to Be Used
+* subject_test.txt
+* y_test.txt
+* X_test.txt
+* subject_train.txt
+* y_train.txt
+* X_train.txt
+* features.txt
+* activity_labels.txt
+
+## Read In Files Common to Both Test and Train.
+* activity_labels.txt
+    * contains data referencing activity id to activity name.
+* features.txt
+    * contains data referencing feature_id to feature name
+
+## Combining Data Inside Test and Train
+Before combining testing and training datasets, we need to first combine
+data within those folders into one.
+
+First, it was unclear from the README what y_train were.  I read them in and did a summary.  Once I realized that the range was 1-6 I realized they were references to labels.
+
+There are three files here:
+* y_train
+    - Label linking the datasets to activity id.
+* X_train
+    - Training data, arranged in columns in the order described in features.txt
+* subject_train
+    - The id of subject
--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,49 @@
 
-## Set WD
 
-setwd("/Users/MrMaksimize/Code/R/coursework/coursera_getdata_14/project")
-
-if (!file.exists('./data')) {
-  if (!file.exists('har_data.zip')) {
-    fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-    download.file(fileUrl, destfile="./har_data.zip", method="curl")
+getData <- function() {
+  if (!file.exists('./data')) {
+    if (!file.exists('har_data.zip')) {
+      fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+      download.file(fileUrl, destfile="./har_data.zip", method="curl")
+    }
+    unzip("./har_data.zip", exdir = ".", overwrite = TRUE)
+    file.rename("./UCI HAR Dataset", "./data")
   }
-  unzip("./har_data.zip", exdir = ".", overwrite = TRUE)
-  file.rename("./UCI HAR Dataset", "./data")
 }
-training <- read.csv('data/train/')
+
+## Set WD to the location of file
+setwd("./")
+
+## Download the data and unzip, if needed.
+getData()
+
+## Read in data common to both test and train.
+
+## Read in Features Labels
+## Assign column names during initial read.
+activity_labels <- read.table(
+  'data/activity_labels.txt',
+  header = FALSE,
+  sep = " ",
+  #row.names = 1,
+  col.names = c("id", "activity_label")
+);
+
+## Read in Features Labels
+## Assign column names during initial read.
+features_labels <- read.table(
+  'data/features.txt',
+  header = FALSE,
+  sep = " ",
+  #row.names = 1,
+  col.names = c("id", "feature_label")
+);
+
+
+
+# Training data only
+train_activities <- read.table(
+  "./data/train/y_train.txt",
+  header = FALSE,
+  sep = " "
+);
