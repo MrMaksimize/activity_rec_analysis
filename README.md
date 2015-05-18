9b39fff7a276979569f725cfce6089ec54e62316
Maksim Pecherskiy <max@maksimize.com>
2015-05-17 20:24:22 -0700

    Finally getting caching to a semi-working state"

--- README.md
+++ README.md
@@ -6,26 +6,31 @@ R Studio Version
 ## Package Deps
 ? Try to install them automatically?
 ## Files to Be Used
-* subject_test.txt
-* y_test.txt
-* X_test.txt
-* subject_train.txt
-* y_train.txt
-* X_train.txt
-* features.txt
-* activity_labels.txt
+* `subject_test.txt`
+* `y_test.txt`
+* `X_test.txt`
+* `subject_train.txt`
+* `y_train.txt`
+* `X_train.txt`
+* `features.txt`
+* `activity_labels.txt`
 
 ## Read In Files Common to Both Test and Train.
-* activity_labels.txt
-    * contains data referencing activity id to activity name.
-* features.txt
-    * contains data referencing feature_id to feature name
+* `activity_labels.txt`
+    * contains data referencing `activity id` to activity name.
+* `features.txt`
+    * contains data referencing `feature_id` to feature name
+
+I read those into a list so they're easier to use.
 
 ## Combining Data Inside Test and Train
 Before combining testing and training datasets, we need to first combine 
 data within those folders into one.
 
-First, it was unclear from the README what y_train were.  I read them in and did a summary.  Once I realized that the range was 1-6 I realized they were references to labels.
+*Also, I'm using `train` below as an example, but the same is true for the `test` folder.*
+
+First, it was unclear from the README what `y_train` were.  I read them in and did a summary.  Once I realized that the range was `1-6` I realized they were references to activity labels (activity_labels.txt).
+
 
 There are three files here:
 * y_train
@@ -33,8 +38,15 @@ There are three files here:
 * X_train
     - Training data, arranged in columns in the order described in features.txt
 * subject_train
-    - The id of subject
+    - The id of subject (1-30)
+
+A few more observations
+* `features.txt` from above has the same number of rows as there are columns in `X_train`.  It makes sense for them to be column labels.
+* `y_train` has the same number of rows as `X_Train` which tells me they are corresponding datasets and need to be combined.
 
 
 
+## Combining Test and Train.
+
+Once
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,12 @@
 library(dplyr)
+library(reshape2)
 
 ## Set WD to the location of file
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 print(getwd())
 
-getData <- function() {
+dlData <- function() {
   if (!file.exists('./data')) {
     if (!file.exists('har_data.zip')) {
       fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
@@ -16,55 +17,98 @@ getData <- function() {
   }
 }
 
+loadCommonData <- function() {
+  ## Read in Features Labels
+  ## Assign column names during initial read.
+  activity_labels <- read.table(
+    './data/activity_labels.txt', 
+    header = FALSE, 
+    sep = " ", 
+    #row.names = 1,
+    col.names = c("id", "activity_label")
+  );
+  
+  ## Read in Features Labels
+  ## Assign column names during initial read.
+  features_labels <- read.table(
+    'data/features.txt', 
+    header = FALSE, 
+    sep = " ", 
+    #row.names = 1,
+    col.names = c("id", "feature_label")
+  );
+  
+  list(activity_labels = activity_labels, features_labels = features_labels);
+}
 
+loadFileCached <- function(setName, reset = FALSE) {
+  varName <- paste0(setName, "_cache")
+  if (nrow(get(varName)) == 0 || reset == TRUE) {
+    set_data <- read.table(paste0("./data/", setName, "/X_", setName, ".txt"), header = FALSE)
+    assign("train_cache", set_data, envir = .GlobalEnv)
+  }
+  else
+    set_data <- get(varName)
+  
+  set_data
+}
+
+getSetData <- function(sets = c("train", "test")) {
+  setList = list()
+  for(setName in sets) {
+    set_activities <- read.table(
+      paste0("./data/", setName, "/y_", setName, ".txt"),
+      header = FALSE, 
+      sep = " ",
+      col.names = c("activity_id")
+    );
+  
+    set_subjects <- read.table(
+      paste0("./data/", setName, "/subject_", setName, ".txt"),
+      header = FALSE, 
+      sep = " ",
+      col.names = c("subject_id")
+    );
+    
+    set_data <- loadFileCached(setName)
+    
+    ## First, apply the features as column names.
+    names(set_data) <- commonData$features_labels$feature_label
+    
+    ## Add subject trained column
+    ## TODO -- should this be cbind?
+    set_data$subject_id <- set_subjects$subject_id
+    
+    ## Add activity id column
+    #set_data$activity_id <- set_activities$activity_id
+    #set_data <- cbind(set_data, set_activities)
+    
+    
+    #set_data <- melt(set_data, id.vars = c("activity_id", "subject_id"))
+    setList[[setName]] <- set_data
+  }
+  setList
+}
+
+## Cache Variables - global
+train_cache <- data.frame()
+test_cache <- data.frame()
 
 ## Download the data and unzip, if needed.
-getData()
+dlData()
 
 ## Read in data common to both test and train.
+commonData <- loadCommonData()
 
-## Read in Features Labels
-## Assign column names during initial read.
-activity_labels <- read.table(
-  './data/activity_labels.txt', 
-  header = FALSE, 
-  sep = " ", 
-  #row.names = 1,
-  col.names = c("id", "activity_label")
-);
+## Read in both sets of data and pre-format them.
+## Takes forever, so don't do it twice!
+#if (!exists("sets"))
+#sets <- getSetData();
 
-## Read in Features Labels
-## Assign column names during initial read.
-features_labels <- read.table(
-  'data/features.txt', 
-  header = FALSE, 
-  sep = " ", 
-  #row.names = 1,
-  col.names = c("id", "feature_label")
-);
+## Merge the training 
 
+#combinedSets <- merge()
 
-
-# Training data only
-train_activities <- read.table(
-  "./data/train/y_train.txt",
-  header = FALSE, 
-  sep = " "
-);
-
-train_subjects <- read.table(
-  "./data/train/subject_train.txt",
-  header = FALSE, 
-  sep = " "
-);
-
-## Use a caching mechanism here. (TODO abstract to fxn, use proper caching)
-if (!exists("train_set")) {
-  train_set <- read.table(
-    "./data/train/X_train.txt",
-    header = FALSE
-  )
-}
 
 
 
